50180272d79f3a9a6dd901cc0226b900bc739b50
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2013-07-22 03:46:51 -0700

    bump version again

--- src/duphunter/__init__.py
+++ src/duphunter/__init__.py
@@ -15,7 +15,7 @@ from threading import Thread, Lock
 import cPickle
 from pkg_resources import resource_filename, DistributionNotFound
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 
 class HashCache:
